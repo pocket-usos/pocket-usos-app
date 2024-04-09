33c9c285d5d505312729f358bbe00e34d89a6568
Andrii <a.vozniuk00@gmail.com>
2024-04-09 17:07:26 +0200

    feat(PU-29): setup push notifications for iOS

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,13 @@ import {Provider} from 'react-redux';
 import {store} from '@store/store';
 import NotificationController from '@modules/Notification/NotificationController.tsx';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {OneSignal} from 'react-native-onesignal';
+import Config from 'react-native-config';
 
 const App = () => {
+  OneSignal.initialize(Config.ONESIGNAL_APP_ID);
+  OneSignal.Notifications.requestPermission(true);
+
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <Provider store={store}>
